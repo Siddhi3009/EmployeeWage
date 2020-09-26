5ff92cf7419e4d2138c1f0637df0279c5ef0a41f
Administrator <siddhiseth3009@gmail.com>
2020-09-26 16:02:18 +0530

    [Siddhi]Refactor Part time employee added

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -16,14 +16,23 @@ namespace EmployeeWage
 
         static int EmployeeDailyWageComputation()
         {
+            int IS_FULL_TIME = 1;
             int IS_PRESENT = 1;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             Random random = new Random();
             int empCheck = random.Next(0,2);
+            int empType = random.Next(0,2);
             if (empCheck == IS_PRESENT)
             {
-                empHrs = 8;
+                if (empType == IS_FULL_TIME)
+                {
+                    empHrs = 8;
+                }
+                else
+                {
+                    empHrs = 4;
+                }
             }
             else
             {
